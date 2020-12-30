2e125711b39ee4985ee1e6f4aeff40a2ea6a092a
Alessia <ale.rainbow96@gmail.com>
2020-12-30 10:37:22 +0100

    aggiunto macelum + aggiustato robe

--- Unity/Assets/Scripts/ArrayModifier.cs
+++ Unity/Assets/Scripts/ArrayModifier.cs
@@ -7,7 +7,6 @@ public class ArrayModifier : MonoBehaviour
     public int CountX;
     public int CountY;
     public GameObject TheObject;
-    public float offset;
     void OnValidate()
     {
         Apply();
@@ -16,7 +15,6 @@ public class ArrayModifier : MonoBehaviour
 
     private void Apply()
     {
-        if (offset == null) offset = 0;
         if (UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode) return;
         if (TheObject == null) return;
         Renderer renderer = TheObject.GetComponent<Renderer>();
@@ -32,19 +30,19 @@ public class ArrayModifier : MonoBehaviour
         }
 
         float lastX = 0;
-        float lastY = 0;
+        float lastZ = 0;
 
         for (int i=0; i<CountY; i++)
         {
             for(int j=0; j <CountX; j++)
             {
-                Vector3 pos = new Vector3(transform.localPosition.x, lastY + transform.localPosition.y, transform.localPosition.z + lastX);
+                Vector3 pos = new Vector3(transform.localPosition.x + lastX, transform.localPosition.y, transform.localPosition.z + lastZ);
                 GameObject go = Instantiate(TheObject, pos, Quaternion.identity, transform) as GameObject;
                 go.name = TheObject.name + "_" + i + "_" + j;
-                lastX -= renderer.bounds.size.z + offset;
+                lastX -= renderer.bounds.size.x;
             }
             lastX = 0;
-            lastY += renderer.bounds.size.y;
+            lastZ += renderer.bounds.size.z;
         }
     }
 }
