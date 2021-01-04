a8c93f68b439fdb6d664af4cedf548508f485a8e
klajdimyftari <klajdi.myftari94@gmail.com>
2021-01-05 00:45:37 +0100

    Lapillo: completato particles, manca: aduio + script luoghi e timer di spawn

--- Unity/Assets/Scripts/ProjectileMoveScript.cs
+++ Unity/Assets/Scripts/ProjectileMoveScript.cs
@@ -6,6 +6,8 @@ public class ProjectileMoveScript : MonoBehaviour
 {
     public float Speed;
     public GameObject Impactprefab;
+    public List<GameObject> Trails;
+
     private Rigidbody rb;
 
     // Start is called before the first frame update
@@ -35,6 +37,20 @@ public class ProjectileMoveScript : MonoBehaviour
         {
             var impactVFX = Instantiate(Impactprefab, pos, rot) as GameObject;
             Destroy (impactVFX, 5);
+        }
+
+        if(Trails.Count > 0)    //faccio durare la scia di fumo dopo che il lapillo ha colpito il terreno
+        {
+            for(int i = 0; i<Trails.Count; i++)
+            {
+                Trails[i].transform.parent = null;
+                var ps = Trails[i].GetComponent<ParticleSystem>();
+                if(ps!=null)
+                {
+                    ps.Stop();
+                    Destroy (ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
+                }
+            }
         }
 
         Destroy (gameObject);
