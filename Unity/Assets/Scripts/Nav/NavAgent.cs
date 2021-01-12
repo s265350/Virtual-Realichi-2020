fe2fcbe2c39bcf88ed569f43fdaddd45ee85b9e0
Francesco Alagna <49679556+s265350@users.noreply.github.com>
2021-01-12 17:58:50 +0100

    NPC stupidi

---
+++ Unity/Assets/Scripts/Nav/NavAgent.cs
@@ -0,0 +1,100 @@
+﻿/*
+    This instanciates a NavAgent object to use as base Agent for NPCs
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavAgent<T>
+{
+    private GameObject _owner;
+    private FiniteStateMachine<T> _stateMachine;
+    private UnityEngine.AI.NavMeshAgent _navMeshAgent;
+
+    public readonly float walkSpeed = 1.5f;
+    public readonly float runSpeed = 3f;
+    public readonly float distanceToStop = 3f;
+
+    private Dictionary<string, State> _states = new Dictionary<string, State>();
+
+    private Vector3 _destination = default(Vector3);
+    private bool _run;
+    private bool _destroy = false;
+
+    public NavAgent(GameObject owner, FiniteStateMachine<T> stateMachine)
+    {
+        _owner = owner;
+        _stateMachine = stateMachine;
+        _navMeshAgent = _owner.GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        // Settings
+        _navMeshAgent.angularSpeed = 1000f;
+        _navMeshAgent.acceleration = 10f;
+        _navMeshAgent.stoppingDistance = distanceToStop;
+
+        // Basic states
+        State move = AddState("Move", () => {}, () => {GoToDestination();}, () => {});
+        State stop = AddState("Stop", () => {_navMeshAgent.isStopped = true;}, () => {if(DestinationIsValid() && _destroy)DestroyOwner();else _destination = default(Vector3);}, () => {_navMeshAgent.isStopped = false;});
+
+        // Basic transitions
+        _stateMachine.AddTransition(move, stop, () => StopAgent());
+        _stateMachine.AddTransition(stop, move, () => !StopAgent());
+
+        // START STATE
+        SetState("Stop");
+    }
+
+    public void Tik() => _stateMachine.Tik();
+
+    public State AddState(string name, System.Action enter, System.Action tik, System.Action exit)
+    {
+        State state = new State(name, _owner, enter, tik, exit);
+        if(_states.TryGetValue(name, out var s)) s = state;
+        else _states.Add(name, state);
+        return state;
+    }
+
+    public void AddTransition(State from, State to, System.Func<bool> condition) => _stateMachine.AddTransition(from, to, condition);
+
+    public List<string> GetStates(){return new List<string>(_states.Keys);}
+
+    public State GetState(string name){if(_states.TryGetValue(name, out State state))return state;return null;}
+
+    public void SetState(string statename)
+    {
+        if(_states.TryGetValue(statename, out State state)) _stateMachine.SetState(state);
+        else Debug.LogError($"Wrong state setup: {statename}");
+    }
+
+    public void ResetState() => _stateMachine.ResetState();
+
+    public bool StopAgent(){return Vector3.Distance(_destination, _navMeshAgent.transform.position) <= distanceToStop;}
+
+    public void SetDestination(Vector3 target, bool run, bool destroy)
+    {
+        if(!UnityEngine.AI.NavMesh.SamplePosition(target, out UnityEngine.AI.NavMeshHit hit, 1.0f, UnityEngine.AI.NavMesh.AllAreas)) return;
+        if(_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance && _navMeshAgent.velocity.sqrMagnitude <= 0f)
+        {
+            _destination = target;
+            _run = run;
+            _destroy = destroy;
+        }
+    }
+
+    public bool DestinationIsValid(){return !_destination.Equals(default(Vector3));}
+
+    public void GoToDestination()
+    {
+        if(!DestinationIsValid()) return;
+        //Debug.Log($"Moving to {_destination}");
+        if(_run) _navMeshAgent.speed = runSpeed;
+        else _navMeshAgent.speed = walkSpeed;
+        _navMeshAgent.SetDestination(_destination);
+    }
+
+    private void DestroyOwner()
+    {
+        GameObject.FindObjectOfType<NavSpawner>().DestroyedAgent(_owner.GetComponent<MonoBehaviour>().GetType().ToString());
+        GameObject.Destroy(_owner);
+    }
+}
---
+++ Unity/Assets/Scripts/Nav/NavAgentGuard.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+
+public class NavAgentGuard : MonoBehaviour
+{
+    private NavAgent<NavAgentGuard> _navAgent;
+    private Animator _animator;
+
+    private List<Vector3> _targets = new List<Vector3>();
+    protected enum Behaviours{Stop, Talk, Move, Path, Interact}
+    private Behaviours behaviour = Behaviours.Stop;
+
+    void Start()
+    {
+        _navAgent = new NavAgent<NavAgentGuard>(gameObject, new FiniteStateMachine<NavAgentGuard>(gameObject));
+        _animator = gameObject.GetComponent<Animator>();
+        AddStatesAndTransitions();
+    }
+
+    void Update()
+    {
+        List<bool> bools = new List<bool>{false, true, false, false};
+        bool b = bools[Random.Range(0, bools.Count)];
+        switch (behaviour)
+        {
+            case Behaviours.Stop: // do nothing
+                break;
+            case Behaviours.Talk:
+                _animator.SetBool("WalkState", false);
+                _animator.SetBool("TalkState", true);
+                _animator.SetInteger("TalkAnimation", Random.Range(0, 2));
+                break;
+            case Behaviours.Move: // reaches a position and destroys itself
+                _animator.SetBool("WalkState", true);
+                _animator.SetBool("TalkState", false);
+                if(gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().speed == _navAgent.walkSpeed)_animator.SetBool("Run", false);
+                else if(gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().speed == _navAgent.runSpeed)_animator.SetBool("Run", true);
+                _navAgent.SetDestination(_targets[Random.Range(0, _targets.Count)], b, true);
+                break;
+            case Behaviours.Path: // follows the path
+                _animator.SetBool("WalkState", true);
+                _animator.SetBool("TalkState", false);
+                if(gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().speed == _navAgent.walkSpeed)_animator.SetBool("Run", false);
+                else if(gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().speed == _navAgent.runSpeed)_animator.SetBool("Run", true);
+                _navAgent.SetDestination(_targets[Random.Range(0, _targets.Count)], b, false);
+                break;
+            case Behaviours.Interact: // interacts with the user and goes back to the previous state
+                _animator.SetBool("WalkState", false);
+                _animator.SetBool("TalkState", true);
+                _navAgent.ResetState();
+                break;
+            default: throw new System.ArgumentOutOfRangeException();
+        }
+        _navAgent.Tik();
+    }
+
+    private void AddStatesAndTransitions()
+    {
+        State interact = _navAgent.AddState("Interact", () => {}, () => {}, () => {});
+        foreach (var item in _navAgent.GetStates())
+        {
+            State state = _navAgent.GetState(item);
+            if(state != null) _navAgent.AddTransition(state, interact, () => {return false;});
+        }
+    }
+
+    public void SetBehaviour(string b)
+    {
+        switch (b)
+        {
+            case "Stop":
+                behaviour = Behaviours.Stop;
+                break;
+            case "Talk":
+                behaviour = Behaviours.Talk;
+                break;
+            case "Move":
+                behaviour = Behaviours.Move;
+                break;
+            case "Path":
+                behaviour = Behaviours.Path;
+                break;
+            case "Interact":
+                behaviour = Behaviours.Path;
+                break;
+            default: throw new System.ArgumentOutOfRangeException();
+        }
+    }
+
+    public void SetTargets(List<Vector3> targets){_targets = new List<Vector3>(targets);}
+}
